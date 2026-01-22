75afe700b7e4224e951b0f0cccbb4db929d88434
sakalauskej-crypto <sakalauske.j@gmail.com>
2026-01-22 20:45:54 +0200

    pridejau formos logika

---
+++ script.js
@@ -0,0 +1,32 @@
+const carForm = document.getElementById("carForm");
+const carsDiv = document.getElementById("cars");
+
+carForm.addEventListener("submit", function(event) {
+    event.preventDefault()
+    console.log("forma paspausta");
+// 
+    const name = document.getElementById("name").value;
+    const description = document.getElementById("description").value;
+    const price = document.getElementById("price").value;
+    const image = document.getElementById("image").value;
+    const fuel = document.getElementById("fuel").value;
+
+    //  sukuriu automobilio kortele
+    const carCard = document.createElement("div");
+    carCard.className = "car-card";
+
+// papildau kortele informacija
+    carCard.innerHTML = `
+    <img src= ${image} alt="car">
+    <h3> ${name}</h3>
+    <p> Automobilio kaina: ${price}</p>
+    <p>Automobilio kuras: ${fuel} </p>
+`;
+carsDiv.appendChild(carCard);
+
+//  atstatyti forma i pradine padeti
+carForm.reset();
+});
+
+
+
